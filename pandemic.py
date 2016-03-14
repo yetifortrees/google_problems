45db0280307c86220eb62e2888ad7c96bff26711
jmschae3 <jmschae3@ncsu.edu>
2016-03-14 00:14:55 -0400

    Comments
    Done: commenting

--- pandemic.py
+++ pandemic.py
@@ -1,24 +1,64 @@
+####
+#This program is a more optimized solver for the following problem for n = [2, 50]:
+#
+#           Imagine a group of n people. Each person can socialize with one other person of their
+#           choosing. That person may or may not choose to socialize back with them, and may instead
+#           socialize with someone else. This eventually results in groups of individuals who are connected
+#           by a network of socialization. Now imagine someone outside this group wants to infect the largest group
+#           of people with a virus. If they are always successful in infecting the largest group of socialized individuals,
+#           what is the expected average number of people they will infect.
+#
+#This problem has 3 layers:
+#   1. how can the people be socialized/grouped?
+#       This problem is answered by finding the possible integer partitions
+#       for the number of people overall.
+#   2. how can the people be arranged in these groups?
+#       This is answered through combinatorics by calculating the number
+#       of ways to split n people into some set of partitions.
+#   3. how many ways can the socialization edges be arranged in each partition?
+#       This is answered through sequence A000435 on the OEIS. The exact reason is unknown to me,
+#       but the number is not as easy to find as one might think because a socialization action is
+#       akin to an edge in a digraph, but the disease can travel backwards along edges, as in an undirected
+#       graph. Thus a graph must be constructed with directed edges with respect to possible configurations, and
+#       then converted to an undirected graph.
+#
+#This program uses the three layers of calculations above to generate the average number of people infected. Above 50 this program
+#will not work because it only stores the first 50 values of the third layer sequence. Additionally, the number of partitions grows exponentially,
+#so runtime scales accordingly.
+####
 from fractions import Fraction
 import math
 s = {}
+#the first 50 values of A000435
 multipliers = [0,1,8,78,944,13800,237432,4708144,105822432,2660215680,73983185000,2255828154624,74841555118992,2684366717713408,103512489775594200,4270718991667353600,187728592242564421568,8759085548690928992256,432357188322752488126152,22510748754252398927872000,1232926106158164038752479600,70864880389206811919012069376,4264956622099218989964380900408,268233854186801904661404949413888,17596516621570611723107254285860000,1202034287378796801763265196064768000,85369694212882788734452615436623131432,6294445634443154805245648414750533484544,481164307787876713983817697855811244891472,38086159100543376291945674612050231296000000,3117962569860399657478478640723143576082043800,263711778692997479722657378560127779200642842624,23019602620026625886784119896351926037410391377792,2071846675499818842878197235287956993753027358752768,192094716812399052493595004779889746503480732173125000,18331598479550821498637618387125492501532509577674752000,1799128306486873097251634624641612544680165700565794891568,181455277812927984131628207045914510546689975831530576019456,18793558774469620804251752127287685035341811749012764504838392,1997481851773698127207374504024299885204574735495384268800000000,217725066338201044536232277151749132601822287693599525820471957600,24322993207498664811770063992025908744449994272086863441366798565376,2783252962715382903543081261008995076675007754537625408175546581970408,326040422290312358312341535486741187518808190457145240569672167271170048,39078734107607748249951890474620593724028205093939481724417008016906250000,4790020369221769566925830345222465070918938379231286729520950331389352345600,600135530341228889141074788642164985737852858110856472692456945542128324281432,76819732139864778254944970153522541437430179776648013928703825117887136209567744,10041823702864167812042682621441259276711025525338945412768700983639957485080366912,1339928530680358664749551425221017908902142581750156543927120170387232522240000000000]
 def answer(n):
+    #ignore trivial cases
     if n == 2:
         return "2/2"
     if n == 3:
         return "3/3"
+    #This is the max number of configurations
     arrangements = int(math.pow(n-1, n))
+    #Used for calculating the arrangements of nodes in partitions
     maximum = int(math.factorial(n))
+    #initialize the top and bottom of the fraction that represents average people infected
     top = 0
     bottom = 0
+    #find the partitions
     loop = partitions(n)
+    #loop through all configurations of partitions
     for i in loop:
+        #partition sizes must be all > 2 to be a valid partition scheme
         if i[0] > 1:
             here = maximum
             repeat = 0
             times = 1
             for j in i:
+                #the math.factorial takes care of node configs in partitions, and the multipliers
+                #account for the number of configs of edges in each partition
                 here = int(here / math.factorial(j)) * multipliers[j - 1]
+                #if there are partitions of the same size that effects
+                #the possibilities and must be accounted for
                 if j == repeat:
                     times += 1
                     here = int(here / times)
@@ -32,6 +72,8 @@ def answer(n):
     #top += int(arrangements * n)
     #print(float(top)/float(bottom))
     return str(Fraction(int(top), int(bottom)))
+#the partitions of n-1
+#credit to David Eppstein
 def partitions(n):
     # base case of recursion: zero is the sum of the empty list
     if n == 0:
@@ -57,5 +99,6 @@ def choose(n, k):
     if k > n:
         return 0
     return int(math.factorial(n)/(math.factorial(k) * math.factorial(n - k)))
+#testing
 for i in range(2, 50):
     print(answer(i))
--- pandemic2.py
+++ pandemic2.py
@@ -1,50 +1,103 @@
+####
+#This program is a brute force solver for the following problem:
+#
+#           Imagine a group of n people. Each person can socialize with one other person of their
+#           choosing. That person may or may not choose to socialize back with them, and may instead
+#           socialize with someone else. This eventually results in groups of individuals who are connected
+#           by a network of socialization. Now imagine someone outside this group wants to infect the largest group
+#           of people with a virus. If they are always successful in infecting the largest group of socialized individuals,
+#           what is the expected average number of people they will infect.
+#
+#This problem has 3 layers:
+#   1. how can the people be socialized/grouped?
+#       This problem is answered by finding the possible integer partitions
+#       for the number of people overall.
+#   2. how can the people be arranged in these groups?
+#       This is answered through combinatorics by calculating the number
+#       of ways to split n people into some set of partitions.
+#   3. how many ways can the socialization edges be arranged in each partition?
+#       This is answered through sequence A000435 on the OEIS. The exact reason is unknown to me,
+#       but the number is not as easy to find as one might think because a socialization action is
+#       akin to an edge in a digraph, but the disease can travel backwards along edges, as in an undirected
+#       graph. Thus a graph must be constructed with directed edges with respect to possible configurations, and
+#       then converted to an undirected graph.
+#
+#This program generates all configurations by representation as an array, converting to an adjacency matrix,
+#and then evaluating the largest partition in each configuration. Beyond n=8 this is exceptionally slow because
+#configurations grow as (n - 1)^n.
+####
 import math
 seen = set()
 maxConf = []
+#the main function to call
 def answer(n):
+    #the largest configuration representation
     global maxConf
+    #what people we have seen
     global seen
+    #what sets of what sizes we have seen
     sets = {}
+    #a count of how many time the max size was some number
     maxes = [0 for i in range(0, n)]
+    #for accounting
     print(n)
+    #we don't care about trivial cases
     if n == 2:
         return "2/1"
+    #the current configuration
     string = [0 for i in range(0, n)]
+    #the max configuration
     maxConf = [n - 2 for i in range(0, n)]
+    #so we can get the max
     maxConf[0] += 1
+    #the top of the fraction
     top = 0
+    #the max number of configurations
     bottom = math.pow(n - 1, n)
+    #while its a valid configuration
     while string != maxConf:
         #print(string)
+        #generate the digraph adjacency matrix
         adjacency = [[0 for i in range(0, n)] for i in range(0, n)]
         for i in range(0, n):
             if string[i] < i:
                 adjacency[i][string[i]] = 1
             else:
                 adjacency[i][string[i] + 1] = 1
+        #convert it to an undirected graph adjacency matrix
+        #this could be one step
         for i in range(0, n):
             for j in range(0, n):
                 if adjacency[i][j] == 1 or adjacency[j][i] == 1:
                     adjacency[i][j] = 1
                     adjacency[j][i] = 1
+        #what people we have seen in this config so far
         seen = set()
+        #the sizes of the partitions in this config
         sizes = []
+        #breadth first search all the people
         for i in range(0, n):
             if i not in seen:
                 sizes.append(BFS(adjacency, i))
         #print(sizes)
         maxHere = max(sizes)
         partition = str(sizes)
+        #add to the top the biggest partition size
         top += maxHere
+        #accounting
         if partition in sets:
             sets[partition] += 1
         else:
             sets[partition] = 1
         maxes[maxHere - 1] += 1
+        #move to next config
         string = increment(list(string), n)
+    #accounting + return
     print(maxes)
     print(sets)
     return str(top) + "/" + str(bottom)
+#breadth first searches an adjacency matrix
+#returns the indices of nodes seen
 def BFS(adjacency, start):
     global seen
     visited, queue =set(), [start]
@@ -57,6 +110,7 @@ def BFS(adjacency, start):
                     queue.append(i)
     seen = seen.union(visited)
     return len(visited)
+#increments the config
 def increment(string, maxVal):
     global maxConf
     string[0] += 1
@@ -67,7 +121,7 @@ def increment(string, maxVal):
             string[count + 1] += 1
             count += 1 
     return string
-    
+#testing
 for i in range(2, 10):
     print(answer(i))
     print("----")   
